5764bcd391d92f3103fc5a8fbac10754e2bd1424
Popeye <127945393+Popeye68@users.noreply.github.com>
2025-05-27 20:41:37 +0530

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
+const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE',
-  id: parseInt(process.env.ID) || 123456789
+  token: process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE',
+  id: isNaN(parsedId) ? 123456789 : parsedId // replace 12345.. with your telegram chat id
 };
